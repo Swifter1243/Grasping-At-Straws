f2d9930a0b799ee3f4095300be78edbaeb3b2551
Swifter <61858676+Swifter1243@users.noreply.github.com>
2025-02-26 10:10:27 -0500

    change "input and output" to just "input"

--- Assets/Scripts/PuzzlePiece.cs
+++ Assets/Scripts/PuzzlePiece.cs
@@ -5,11 +5,11 @@ namespace SLC_GameJam_2025_1
 {
     public class PuzzlePiece : MonoBehaviour
     {
-        public Transform m_in;
-        public Transform m_out;
+        public Transform m_input1;
+        public Transform m_input2;
 
-        private readonly static Color s_inColor = new(0, 0.5f, 0.4f);
-        private readonly static Color s_outColor = new(1, 0.4f, 0);
+        private readonly static Color s_input1Color = new(0, 0.5f, 0.4f);
+        private readonly static Color s_input2Color = new(1, 0.4f, 0);
 
         public Vector3Int GetPiecePosition() => new()
         {
@@ -20,12 +20,12 @@ namespace SLC_GameJam_2025_1
 
         private void OnDrawGizmos()
         {
-            Debug.DrawLine(transform.position, transform.position + InDirection, s_inColor);
-            Debug.DrawLine(transform.position, transform.position + OutDirection, s_outColor);
+            Debug.DrawLine(transform.position, transform.position + Input1Direction, s_input1Color);
+            Debug.DrawLine(transform.position, transform.position + Input2Direction, s_input2Color);
         }
 
-        public Vector3Int InDirection => GetChildDirection(m_in);
-        public Vector3Int OutDirection => GetChildDirection(m_out);
+        public Vector3Int Input1Direction => GetChildDirection(m_input1);
+        public Vector3Int Input2Direction => GetChildDirection(m_input2);
 
         private Vector3Int GetChildDirection(Transform child)
         {
